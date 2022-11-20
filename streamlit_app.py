5f7d2fc6e87ed4e49ce71ce0d7f2eb3efe081dc8
Minky <118648311+snowminky@users.noreply.github.com>
2022-11-19 22:51:39 -0700

    Update streamlit_app.py
    Added preselects

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,6 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index("Fruit")
 
 # Picklist here so they can pick the furit they want to include
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 
 streamlit.dataframe(my_fruit_list)
