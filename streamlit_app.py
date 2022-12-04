dfd3345d485ae3729cb978ab76fc4f51d4a00733
Minky <118648311+snowminky@users.noreply.github.com>
2022-12-04 16:33:11 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ fruit_choice = stramlit.text.input('What fruit would you like information about?
 streamlit.write('The user entered', fruit_choice)
 
 import requests 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json()) # Asked to remove this line after line 38 -- Removes the json data format line
 
 
