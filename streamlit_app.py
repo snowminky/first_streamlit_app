452f069c19174f8a9b5f66e0e23210ad7da65d4d
Minky <118648311+snowminky@users.noreply.github.com>
2022-12-04 16:30:45 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 #added after line 38
 fruit_choice = stramlit.text.input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered',' fruit_choice)
+streamlit.write('The user entered', fruit_choice)
 
 import requests 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
