0a2d2c03918e498d3a9bcfc69a19f9fb4623f2d7
Minky <118648311+snowminky@users.noreply.github.com>
2022-12-04 15:41:05 -0700

    Update streamlit_app.py
    Lesson 9 using APIs & Variables

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,3 +18,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
+
+
+
+#New Section to display fruityvice api response
+import requests 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
