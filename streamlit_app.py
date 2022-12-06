ec926e65a6e8533732ebdd239648ec8791968ae4
Minky <118648311+snowminky@users.noreply.github.com>
2022-12-05 23:54:43 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,8 +45,16 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur=my_cnx.cursor()
 #my_cur.execute("select current_user(), Current_Account(), Current_Region()")
 my_cur.execute("select * from fruit_load_list")
+#my_data_rows = my_cur.fetchall()
 my_data_row = my_cur.fetchone()
 #streamlit.text("The fruit load list contains:")
 streamlit.header("The fruit load list contains:")
 #streamlit.text(my_data_row)
-streamlit.dataframe(my_data_row)
+#streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
+
+
+
+
+
+
