b6493ef34a1e94a58cf692c02c59ad2214b1085d
Minky <118648311+snowminky@users.noreply.github.com>
2022-12-06 23:36:30 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,9 +27,7 @@ streamlit.dataframe(fruits_to_show)
 
 #create the repeatable code block - FUNCTION
 def get_fruityvice_data(this_fruit_choice):
-	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-		#streamlit.text(fruityvice_response.json()) # Asked to remove this line after line 38 -- Removes the json data format line
-		#take the json version of the respone and normalize it - put it into a table
+	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice) 
 	fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 	return fruityvice_normalized
 
